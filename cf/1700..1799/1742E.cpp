71a600295ac31f7dc3bc12124976d6ea07dbf694
wylited <44154103+wylited@users.noreply.github.com>
2024-03-26 20:13:48 +0800

    cleaning up

--- cf/1700..1799/1742E.cpp
+++ cf/1700..1799/1742E.cpp
@@ -4,10 +4,34 @@ using namespace std;
 
 typedef long long ll;
 
+#define pb(x) push_back(x);
+
+void solve() {
+  ll n, q, x;
+  cin >> n >> q >> x;
+  vector<ll> a{0, x};
+  vector<ll> pm{x};
+
+  for (int i = 1; i < n; i++) {
+    cin >> x;
+    pm.push_back(max(x, pm.back()));
+    a.push_back(x + a.back());
+  }
+
+  for (int i = 0; i < q; i++) {
+    int k;
+    cin >> k;
+    int pos = upper_bound(pm.begin(), pm.end(), k) - pm.begin();
+    cout << a[pos] << " ";
+  }
+  cout << endl;
+}
+
 int t;
 
 int main() {
   cin >> t;
   while (t--) {
+    solve();
   }
 }
